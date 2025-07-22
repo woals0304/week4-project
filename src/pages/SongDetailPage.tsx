04959a63fa9cb8e2e8ac48e9b8b7d5cffaa61e56
남궁재민 <144192321+woals0304@users.noreply.github.com>
2025-07-23 00:30:13 +0900

    파일 구조 정리 및 중복 제거

--- src/pages/SongDetailPage.tsx
+++
@@ -1,9 +0,0 @@
-@@ .. @@
-             <div className="bg-gray-100 rounded-lg overflow-hidden">
-               <YouTubePlayer
-                 videoId={videoId}
-                 className="w-full aspect-video"
--                onTimeUpdate={setCurrentTime} // YouTubePlayer가 이 prop을 지원한다고 가정
-+                onTimeUpdate={setCurrentTime}
-               />
-             </div>
